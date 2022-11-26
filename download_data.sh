53e899c0e7ee3b5a2e4fa79e24cd2788543b4195
liruiw <liruiw@mit.edu>
2022-11-26 10:23:58 -0500

    update data download script

--- download_data.sh
+++ download_data.sh
@@ -1,5 +1,5 @@
 # Download the data
-wget --no-check-certificate -r 'https://drive.google.com/uc?export=download&id=1tHPAQ2aPdkp8cwtFP4gs4wdcP02jfGpH' -O data.zip
+gdown 'https://drive.google.com/uc?export=download&id=1tHPAQ2aPdkp8cwtFP4gs4wdcP02jfGpH' -O data.zip
 echo "Data downloaded. Starting to unzip"
 unzip -q data.zip  
 rm data.zip
